4d149e24bda6e4ae0b65d7c4a2effbb02b1e9b9b
Patrick <patrickjohnsilvestre@gmail.com>
2020-11-17 22:16:09 -0800

    Format starter files

--- src/utility.h
+++ src/utility.h
@@ -3,10 +3,8 @@
 
 #include "ofMain.h"
 
-bool rayIntersectPlane(const ofVec3f &rayPoint, const ofVec3f &raydir, ofVec3f const &planePoint,
-	const ofVec3f &planeNorm, ofVec3f &point);
+bool rayIntersectPlane(const ofVec3f &rayPoint, const ofVec3f &raydir,
+                       ofVec3f const &planePoint, const ofVec3f &planeNorm,
+                       ofVec3f &point);
 
 ofVec3f reflectVector(const ofVec3f &v, const ofVec3f &normal);
-
-
-
